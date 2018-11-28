9358a6c284e032871cbaecca00575ceedea0fa5a
nishadtupe <36711752+nishadtupe@users.noreply.github.com>
2018-11-28 14:05:13 -0500

    Renamed the file

--- project-report/bin/setup.sh
+++ project-report/bin/setup.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 set -e
 
-# The script installs MongoDB on ubuntu 18.04 and imports the dataset to MongoDB locally
+# The script installs MongoDB on ubuntu 18.04 and download and imports the dataset to MongoDB.
 # The script must run as root user or user that has sudo privilege to install 
-# Script assumes the dataset is in same directory as the script,if not change the dir path
+# Script assumes the dataset is in same directory of  the script,if not change the dir path
 # Ensure mongo_install.sh has execute (chmod + mongo_install.sh)
 
 export LOGFILE1="master.log"
 export LOGFILE2="status.log"
+export LOGFILE3="kaggle.log"
 
-echo "Script Installs Latest Mongodb DB on ubuntu 18.04"
+echo "download the dataset from kaggle.com"
+kaggle datasets download kemical/kickstarter-projects -f ks-projects-201801.csv >> $LOGFILE3
+
+echo "Script Installs Latest Mongodb DB on ubuntu 18.04 Bionic"
 
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 2930ADAE8CAF5059EE73BB4B58712A2291FA4AD5
 
@@ -22,11 +26,21 @@ echo "Install MongoDB package"
 sudo apt install -y mongodb >> $LOGFILE1
 
 echo "Check the Service and Database"
+
+
+#sudo systemctl enable mongod
+
+#sudo service mongod restart
+
 sudo systemctl status mongodb > $LOGFILE2
 
-mongo --eval 'db.runCommand({ connectionStatus: 1 })' >> $LOGFILE2
+# mongo --eval 'db.runCommand({ connectionStatus: 1 })' >> $LOGFILE2
+
+#sudo systemctl restart mongodb  >> $LOGFILE2
 
 echo "Import the database "
+
 mongoimport -d kickstarter -c projects --type csv --file kickproject.csv --headerline >> $LOGFILE2
+
 echo "Import Completed"
 
