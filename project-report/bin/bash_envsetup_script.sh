800301d7cde0ccb63b8cf9112ecc983d42f24f29
Nishad Tupe <nishad.tupe@cummins.com>
2018-12-02 20:55:21 -0500

    updated code with Mongo Cloud credentials

--- project-report/bin/bash_envsetup_script.sh
+++ project-report/bin/bash_envsetup_script.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -e
 
-# The script installs MongoDB on ubuntu 18.04 and download and imports the dataset to MongoDB.
-# The script must run as root user or user that has sudo privilege to install 
-# Script assumes the dataset is in same directory of  the script,if not change the dir path
+# The script installs MongoDB on ubuntu 18.04 and download and imports the dataset(kaggle) to MongoDB.
+# The script must run as root user or user that has sudo privileges to install 
+# Script assumes the dataset is in same directory of the script,if not change the dir path
 # Ensure mongo_install.sh has execute (chmod + mongo_install.sh)
 
 export LOGFILE1="master.log"
@@ -11,6 +11,7 @@ export LOGFILE2="status.log"
 export LOGFILE3="kaggle.log"
 
 echo "download the dataset from kaggle.com"
+echo "if kaggle api package not installed use pip install kaggle" 
 kaggle datasets download kemical/kickstarter-projects -f ks-projects-201801.csv >> $LOGFILE3
 
 echo "Script Installs Latest Mongodb DB on ubuntu 18.04 Bionic"
@@ -22,21 +23,15 @@ echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongod
 echo "Find & Update the latest packages"
 sudo apt-get update > $LOGFILE1
 
-echo "Install MongoDB package"
+echo "Install MongoDB package default installs the latest version"
 sudo apt install -y mongodb >> $LOGFILE1
 
 echo "Check the Service and Database"
 
-
-#sudo systemctl enable mongod
-
-#sudo service mongod restart
-
 sudo systemctl status mongodb > $LOGFILE2
 
-# mongo --eval 'db.runCommand({ connectionStatus: 1 })' >> $LOGFILE2
-
-#sudo systemctl restart mongodb  >> $LOGFILE2
+echo "records the mongodb status using mongo shell"
+mongo --eval 'db.runCommand({ connectionStatus: 1 })' >> $LOGFILE2
 
 echo "Import the database "
 
--- project-report/bin/main.py
+++ project-report/bin/main.py
@@ -1,6 +1,7 @@
+''' This file is mainly used for performance benchmarking '''
+
 import matplotlib.pyplot as plt
 import pandas as pd
-import numpy as np
 import seaborn as sns
 from collections import Counter
 from datetime import datetime
@@ -8,49 +9,8 @@ import pymongo
 from pymongo import MongoClient
 from wordcloud import WordCloud
 import time
-import re
-from   datetime import datetime
-import logging 
-
-
-def load_data():
-    #client = MongoClient()
-    #db = client.kickstarter
-    #collection = db.projects
-    #df = pd.DataFrame(list(collection.find()))
-      
-    client = MongoClient('mongodb+srv://nishad:****@cluster0-4fgwj.azure.mongodb.net/test?retryWrites=true')
-    db = client.kick
-    collection = db.project
-    df = pd.DataFrame(list(collection.find()))
-    
-    
-    '''Data Cleaning Features '''
-    df.drop(['usd pledged'], axis=1, inplace=True)
-    df.replace('N,0"','NO',inplace=True)
-    
-    '''extract features'''
-    
-    ''' Launched date conversion to day,year,month,time'''
-    df['year'] = df['launched'].apply(lambda x:x[0:4])                                                                                                                                                                                                                                          
-    df['month'] = df['launched'].apply(lambda x:x[5:7])
-    df['day'] = df['launched'].apply(lambda x:x[8:10])
-    df['time'] = df['launched'].apply(lambda x:x[11:])
-    
-    ''' Deadline date conversion to day,year,month,time'''
-    df['year_d'] = df['deadline'].apply(lambda x:x[0:4])                                                                                                                                                                                                                                          
-    df['month_d'] = df['deadline'].apply(lambda x:x[5:7])
-    df['day_d'] = df['deadline'].apply(lambda x:x[8:10])
-    df['time_d'] = df['deadline'].apply(lambda x:x[11:])
-    
-    ''' Adding Duration Column '''
-    df['A'] = pd.to_datetime(df['deadline'])
-    df['B'] = pd.to_datetime(df['launched'])
-    df['duration'] = df['A'] - df['B']
-    df.duration = df.duration.dt.days
-    
-    return df
-    
+import numpy as np
+import time 
 def show_wordcloud(data, title = None):
     '''Split names by space and generate word counts.'''
     wordcloud = WordCloud(
@@ -61,123 +21,127 @@ def show_wordcloud(data, title = None):
         random_state=1 # chosen at random by flipping a coin it was heads
     ).generate(str(data))
 
-    fig = plt.figure(1, figsize=(12, 12))
+    fig = plt.figure(figsize =(10,10))
     plt.axis('off')
     if title: 
         fig.suptitle(title, fontsize=20)
         fig.subplots_adjust(top=2.3)
 
     return plt.imshow(wordcloud)
+
+def load_data():
     
+    #df = pd.read_csv("ks-projects-201801.csv")
+    
+    ''' Mongo DB Atlas connection '''
+    
+    client = MongoClient('mongodb+srv://nishad:Tesla18$@cluster0-4fgwj.azure.mongodb.net/test?retryWrites=true')
+    db = client.kick
+    collection = db.project
+    df = pd.DataFrame(list(collection.find()))
+    
+    ''' If want to use DigitalOcean please use below connection string '''
+    # client = pymongo.MongoClient("mongodb://nishad:Tesla18$@159.203.170.43/kick")
+
+
+    #df.drop(['usd pledged'], axis=1, inplace=True)
+
+    df.replace('N,0"','NO',inplace=True)
+
+    ''' Launched date conversion to day,year,month,time'''
+    df['year'] = df['launched'].apply(lambda x:x[0:4])                                                                                                                                                                                                                                          
+    df['month'] = df['launched'].apply(lambda x:x[5:7])
+    df['day'] = df['launched'].apply(lambda x:x[8:10])
+    df['time'] = df['launched'].apply(lambda x:x[11:])
+
+    ''' Deadline date conversion to day,year,month,time'''
+    df['year_d'] = df['deadline'].apply(lambda x:x[0:4])                                                                                                                                                                                                                                          
+    df['month_d'] = df['deadline'].apply(lambda x:x[5:7])
+    df['day_d'] = df['deadline'].apply(lambda x:x[8:10])
+    df['time_d'] = df['deadline'].apply(lambda x:x[11:])
+
+    df['A'] = pd.to_datetime(df['deadline'])
+    df['B'] = pd.to_datetime(df['launched'])
+    df['duration'] = df['A'] - df['B']
+    df.duration = df.duration.dt.days
+    return df
+
+
 def main():
-    
-    '''measure Performance'''
-    perflog = open('performance.log','w')
-    start_time = time.time()
-    perflog.write("Program started at: ")
-    perflog.write(str(start_time))
-    
-    df = load_data()
-    #plt.style.use('ggplot')
-    plt.figure(figsize=(12,8))
-    sns.set(style="white")
-    
-  
+    start = time.time()
     ''' Projects statuses '''
+    df = load_data()
+    plt.style.use('ggplot')
+    fig1 = plt.figure(figsize=(12,8))
     df['state'].value_counts().plot(kind='bar')
-    plt.savefig('Project states.png')
-    
-    '''Top Countries with goal'''
-    sns.set(style="white")
-    df_ggsf = df[(df['state'] == 'successful') | (df['state'] =='failed') ]
-    df_gg = df_ggsf[['goal','state','main_category']]
-    df_ggrp= df_gg.groupby(['main_category','state'],as_index=False).mean().sort_values(by='goal',ascending=False)
-    plt.figure(figsize=(15,9))
-    sns.barplot(x='goal',y='main_category',data=df_ggrp,hue='state',palette='viridis')
-    plt.legend(loc='center right')
-    plt.savefig('Top countries average goal for Success and failed.png')
- 
-    ''' Year wise successful and failed project '''
+    plt.title('project_count_by_status')
+    fig1.savefig('project_states_count.png')
+
+
+    sns.set_style('white')
+    fig2 = plt.figure(figsize=(10,6))
     df3 = df[(df['state']=='successful')| (df['state']=='failed')].sort_values(by='pledged',ascending=True)
     sns.countplot(x='year',hue='state',data=df3)
-    plt.title('Project_count_across_different_states')
-    plt.savefig('Project_count_across_different_states.png')
-    
-    '''Project status across categories'''
+    plt.title('project_count_by_year')
+    fig2.savefig('project_count_by_year.png')
+
+
+    sns.set_style('white')
+    fig3 = plt.figure(figsize=(10,6))
     df[['main_category', 'state']].pivot_table(columns='state', index='main_category', aggfunc=np.size).plot(kind='bar', figsize=(12, 8), stacked=True)
-    plt.savefig('Project_count_across_different_states.png')
-    
-    ''' Top Markets ''' 
+    plt.title('project_states_over_main_category')
+    fig3.savefig('project_states_by_category.png')
+
+
+    fig4 = plt.figure(figsize=(10,6))
     df_s = df[(df['state']=='successful')| (df['state']=='failed')]
-    df_c = df_s[['pledged','state','country']]
-    df_cp = df_c.groupby(['country','state'],as_index=False).mean().sort_values(by='pledged',ascending=False)
-    sns.barplot(x='country',y='pledged',data=df_cp,hue='state',palette='muted')
-    plt.savefig('Top Markets.png')
-    
-    '''Heatmap of pledge amount'''
-    df_gn = df[['pledged','state','main_category']]
+    df_c = df_s[['usd pledged','state','country']]
+    df_cp = df_c.groupby(['country','state'],as_index=False).mean().sort_values(by='usd pledged',ascending=False)
+    sns.barplot(x='country',y='usd pledged',data=df_cp,hue='state',palette='muted')
+    plt.title('top_global_markets_for_pledged')
+    fig4.savefig('top_markes_for_pledged.png')
+
+    fig5 = plt.figure(figsize=(10,6))
+    df_gn = df[['usd_pledged_real','state','main_category']]
     df_gn_g = df_gn.groupby(['main_category','state'],as_index=False).mean()
     df_pivot = df_gn_g.pivot(index='state',columns='main_category')
     df_pivot.dropna(inplace=True)
-    plt.figure(figsize=(15,8))
-    sns.heatmap(df_pivot,cmap='viridis')
+    labels = ['art','comics','crafts','dance','design','fashion','film & video','food','games','journalism','music','photography','publishing','technology','theater']
+    g = sns.heatmap(df_pivot,cmap='viridis',xticklabels=labels)
+    #plt.xticks(np.arange(0, 1, step=0.5))
+    plt.xticks(rotation=45)
+    plt.title('heatmap:main_category,state & plegded_amount')
+    fig5.savefig('heatmap.png')
 
-    ''' Average Pledged Amount for Success and Failed Projects'''
-    df_sf = df[(df['state']=='successful')| (df['state']=='failed')]
-    df_g = df_sf[['pledged','state','main_category']]
-    df_grp= df_g.groupby(['main_category','state'],as_index=False).mean().sort_values(by='pledged',ascending=False)
-    sns.barplot(x='main_category',y='pledged',data=df_grp,hue='state',palette='muted')
-    plt.savefig('Pledge_Amount_Average.png')
-    
-    ''' SNS dist plots continuous variables '''
-    '''goal'''
-    dims = (14, 8)
-    fig, (ax1, ax2) = plt.subplots(ncols=2, sharey=False, figsize=dims)
+   
+    dims = (10,6)
+    fig6, (ax1, ax2) = plt.subplots(ncols=2, sharey=False, figsize=dims)
     sns.distplot(df.goal, ax=ax1)
     sns.distplot(np.log1p(df.goal), ax=ax2)
-    plt.savefig('Dist_plot_goal.png')
-    
-    '''pledged''' 
-    dims = (14, 8)
-    fig, (ax1, ax2) = plt.subplots(ncols=2, sharey=False, figsize=dims)
-    sns.distplot(df.pledged, ax=ax1)
-    sns.distplot(np.log1p(df.pledged), ax=ax2)
-    plt.savefig('Dist_plot_pledged.png')
-    
-    '''Amount Remaining in failed categories'''
-    df = df.loc[df['state'] == 'failed'] 
-    df['remain'] = df['goal']-df['pledged']
-    df8 = (df.groupby(['main_category']).agg({'goal':'sum', 'pledged':'sum', 'remain':'sum'}).reset_index())
+    plt.title('distplot_goal')
+    fig6.savefig('distplot_goal.png')
+
+
+    fig7 = plt.figure(figsize=(10,6))
+    df_r = df[df['state'] == 'failed'] 
+    df_r['remain'] = df_r['goal']- df_r['pledged']
+    df8 = (df_r.groupby(['main_category']).agg({'goal':'sum', 'pledged':'sum', 'remain':'sum'}).reset_index())
+    #print(df8)
     ax = plt.axes()
-    ax.bar(df8.main_category, df8['remain'], width=0.4, color='r',label = 'Remaining')
-    ax.bar(df8.main_category, df8['pledged'], width=0.4, color='g', label = 'Pledged')
-    plt.xticks(rotation=90)
+    ax.bar(df8.main_category, df8['remain'], width=0.4, color='r',label = 'remaining')
+    ax.bar(df8.main_category, df8['pledged'], width=0.4, color='g', label = 'pledged')
+    plt.xticks(rotation=45)
     ax.legend(fontsize = 10)
-    plt.title('Amount Remaining in failed categories')
-    plt.savefig('Amount_Remaining_in_failed_categories.jpg')
-   
-    
-    '''WordCloud''' 
-    show_wordcloud(df[df.state == 'successful']['category'])
-    start_time = time.time()
-    
-    ''' boxplot '''
-    df4 = df.groupby('state').duration.mean().sort_index()
+    plt.title('failure_categories_pledged_remaining_fund.png')
+    fig7.savefig('failure_categories_pledged_remaining_fund.jpg')
+
+    fig8 = plt.figure(figsize=(10,6))
+    df4  = df.groupby('state').duration.mean().sort_index()
     df4.plot(kind='box')
+    plt.title('duration_box_plot')
+    fig8.savefig('duration_box_plot.png')
     
-    end_time= time.time() - start_time
-    perflog.write("Program ended with: ")
-    perflog.write(str(end_time))
-    perflog.close()
-    
-if __name__ == "__main__":
-   e_logger = logging.getLogger('exceptionlogger')
-   e_logger.setLevel(logging.DEBUG) 
-   formatter = logging.Formatter("%(asctime)s:%(message)s")
-   e_handler = logging.handlers.RotatingFileHandler("exception.log",maxBytes=10000,backupCount=5)
-   e_handler.setFormatter(formatter)
-   e_logger.addHandler(e_handler)
-   try:
-        main()
-   except Exception:
-        e_logger.exception("message")
+    show_wordcloud(df[df.state == 'successful']['category'])
+    end = time.time()
+    print(end - start)
+main()
